17ee0c89c540e4600c93f59e89dc631717cc7f7c
marcinrejdych <marcin.rejdych@relayr.io>
2018-01-21 00:05:49 +0100

    mr: added swagger, changes in build.gradle, dockerfiles, docker-compose and configuration

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM openjdk:8
 
-WORKDIR /opt/jalgoarena-eureka
-ADD build/libs/jalgoarena-eureka-1.0.4.jar /opt/jalgoarena-eureka
+
+ADD build/libs/jalgoarena-eureka-*.jar /opt/jalgoarena-eureka/jalgoarena-eureka.jar
 
 EXPOSE 5000
 
-CMD java -Dserver.port=5000 -jar /opt/jalgoarena-eureka/jalgoarena-eureka-1.0.4.jar
+ENTRYPOINT java -Dserver.port=5000 -jar /opt/jalgoarena-eureka/jalgoarena-eureka.jar
--- src/main/kotlin/com/jalgoarena/JAlgoArenaEurekaServer.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaEurekaServer.kt
@@ -4,12 +4,28 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableHystrixDashboard
-open class JAlgoArenaEurekaServer
+@Configuration
+@EnableSwagger2
+open class JAlgoArenaEurekaServer {
 
+    @Bean
+    open fun jalGoArenaApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.jalgoarena"))
+                .paths(PathSelectors.any()).build()
+    }
+}
 fun main(args: Array<String>) {
     SpringApplication.run(JAlgoArenaEurekaServer::class.java, *args)
 }
